3dfd77153f97b5c55d992a1fb7ce629f9d7850f9
Alanportodeveloper <97985674+Alanportodeveloper@users.noreply.github.com>
2026-01-30 23:31:19 +0000

    feat: Foi criado as funções de limpar o buffer e de menu principal assim como o print inicial

--- war.c
+++ war.c
@@ -6,11 +6,33 @@
 
 #define TAM_NOME  30
 #define TAM_COR 10
+#define MAX_TERRITORIOS 5
 
 // Definição da estrutura Território
 
-struct Territorio {
+struct TERRITORIO{
    char nome[TAM_NOME];
    char cor[TAM_COR];
    int tropas;
 };
+
+// Função para limpar o Buffer de entrada
+
+void limparBufferEntrada() {
+int c;
+while ((c = getchar()) != '\n' && c != EOF);
+
+}
+
+// Função principal
+
+int main () {
+    struct TERRITORIO territorios[MAX_TERRITORIOS];
+
+    
+// Exibe o Menu inicial
+printf("=========================================================\n\n");
+printf("Vamos cadastrar os 5 territórios iniciais do nosso mundo\n\n"); 
+
+}
+
