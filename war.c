83de7035bb180cabcbbb2a85be89dedf8bc93ef3
Alanportodeveloper <97985674+Alanportodeveloper@users.noreply.github.com>
2026-01-30 22:37:42 +0000

    feat: Foi criado e atribuido valores para as variáveis globais  e criado a estrutura de território

--- war.c
+++ war.c
@@ -1,3 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+// Contantes globais 
+
+#define TAM_NOME  30
+#define TAM_COR 10
+
+// Definição da estrutura Território
+
+struct Territorio {
+   char nome[TAM_NOME];
+   char cor[TAM_COR];
+   int tropas;
+};
