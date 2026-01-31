13696ab6138844f946612970d91161630aa0354a
Alanportodeveloper <97985674+Alanportodeveloper@users.noreply.github.com>
2026-01-31 00:07:24 +0000

    feat: Contruçao do laço for

--- war.c
+++ war.c
@@ -34,5 +34,21 @@ int main () {
 printf("=========================================================\n\n");
 printf("Vamos cadastrar os 5 territórios iniciais do nosso mundo\n\n"); 
 
+do
+
+
+{
+    for (int i = 0; i < MAX_TERRITORIOS; i++){
+        printf("--- Cadastrando Território %d --- \n", i + 1 );
+        printf("Nome do território: %s \n", territorios[i].nome);
+        printf("Cor do exécito (ex: Azul, Verde): %s \n", territorios[i].cor);
+        printf("Número de Tropas: %d \n\n", territorios[i].tropas);
+    }
+    
+} while (MAX_TERRITORIOS < 5);
+
+
+
+return 0; // Fim do programa
 }
 
